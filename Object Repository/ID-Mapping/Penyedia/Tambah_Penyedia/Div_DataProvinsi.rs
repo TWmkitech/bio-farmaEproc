02cb02c27269682c1e3e6c3dff09a21588d5d445
DESKTOP-50CMDJ4\ASUS <tw.mkitech.01@gmail.com>
2024-02-13 17:44:44 +0700

    Commit - Ruli

---
+++ Object Repository/ID-Mapping/Penyedia/Tambah_Penyedia/Div_DataProvinsi.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Div_DataProvinsi</name>
+   <tag></tag>
+   <elementGuidId>3e0f4166-cd6e-4b6a-9a96-ed635730b06f</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>/html/body/div/div[1]/div[2]/div[3]/div[3]/div/div/div/form/div[12]/div/div[1]/div[2]/div/div/div[10]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
