7b14d0d55eb5190693e7ef401b3f3ffefcf9b504
Acer nitro 5 <Acer nitro 5@DESKTOP-8AT1MI7>
2024-02-13 10:59:07 +0700

    Alfian

---
+++ Object Repository/Third Party/Dalam Negeri/Field_BentukPerusahaan.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Field_BentukPerusahaan</name>
+   <tag></tag>
+   <elementGuidId>8a3383eb-6335-4508-a98b-12945e2714ab</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>/html/body/div[1]/div/div/div[1]/div/div/form/div[3]/div[1]/div/div[1]/div[2]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/Third Party/Dalam Negeri/h5_Akun Telah Berhasil Di Aktivasi.rs
@@ -0,0 +1,115 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>h5_Akun Telah Berhasil Di Aktivasi</name>
+   <tag></tag>
+   <elementGuidId>0073bed1-6212-4edd-bc3f-2bb66b88af5c</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='root']/div/div/div/div/div/center/h5</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>h5.text-white.text-center.mt-3</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>h5</value>
+      <webElementGuid>b3c98da7-d637-45e9-99f4-074c1918ab48</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>text-white text-center mt-3</value>
+      <webElementGuid>0b0de8e3-cc7a-4273-8aff-fe59eab58149</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Akun Telah Berhasil Di Aktivasi!</value>
+      <webElementGuid>f6bf1bdb-ffe3-4dbc-86ee-c63138468203</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;root&quot;)/div[@class=&quot;mb-40 container&quot;]/div[@class=&quot;mt-40 row&quot;]/div[@class=&quot;col-md-6&quot;]/div[@class=&quot;card&quot;]/div[1]/center[1]/h5[@class=&quot;text-white text-center mt-3&quot;]</value>
+      <webElementGuid>ddcb4287-4a6f-4d39-a481-13eda04095d5</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='root']/div/div/div/div/div/center/h5</value>
+      <webElementGuid>8cc53429-04a3-4c82-b149-ee79867ce0be</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Daftar'])[1]/following::h5[1]</value>
+      <webElementGuid>8ea524e7-1d93-4c32-9da4-a3ea9acd1ad4</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Masuk'])[1]/following::h5[1]</value>
+      <webElementGuid>29d75951-4053-4c3c-9cec-be7d7cef2f82</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Proses Calon Penyedia'])[1]/preceding::h5[1]</value>
+      <webElementGuid>cc38da6a-c9fe-44f4-94b3-6d5a1bce88d2</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Pertama'])[1]/preceding::h5[1]</value>
+      <webElementGuid>7cff234a-da5f-4bf5-92e1-575c9ded9af6</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Akun Telah Berhasil Di Aktivasi!']/parent::*</value>
+      <webElementGuid>ce508488-3c25-44a2-8936-4cc51ba8a2f9</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//h5</value>
+      <webElementGuid>34576054-d2e9-43fc-89ee-864547f28474</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//h5[(text() = 'Akun Telah Berhasil Di Aktivasi!' or . = 'Akun Telah Berhasil Di Aktivasi!')]</value>
+      <webElementGuid>d15806e5-b851-429a-a5d5-ea79101d7c23</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
